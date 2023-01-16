a41878f3028b7f5b10efb9a0803488dab2e76b60
Syvertson-Technologies <122764372+Syvertson-Technologies@users.noreply.github.com>
2023-01-16 08:02:06 -0500

    Main.Py Upload
    *revision as last upload was html format

---
+++ main.py
@@ -0,0 +1,33 @@
+import openai
+
+openai.api_key = "ENTER OPENAI API KEY HERE"
+
+# Use the ChatGPT model to generate a response to a given prompt
+def generate_response(prompt):
+    model_engine = "text-davinci-003"
+    prompt = (f"{prompt}\n"
+             )
+
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message.strip()
+
+while True:
+    query = input("> ")
+    # Generate a response using the ChatGPT API
+    response_text = generate_response(query)
+    response_text = response_text.capitalize()
+    print(response_text)
+
+
+# MIT License
+
+# Code under Syvertson-Technologies, written by Devon Chase and revised by ChatGPT
